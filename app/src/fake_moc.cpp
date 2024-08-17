9be350b22964f4d6be9eba65d1289fa8e9f8ea1a
hypengw <hypengwip@gmail.com>
2024-08-18 02:24:29 +0800

    [app] fix fake moc

--- app/src/fake_moc.cpp
+++ app/src/fake_moc.cpp
@@ -4,9 +4,9 @@
 // this should not be compiled
 static_assert(false);
 
-#include <qcm_interface/moc_enum.cpp>
-#include <qcm_interface/moc_global.cpp>
-#include <qcm_interface/moc_model.cpp>
-#include <qcm_interface/model/moc_user_account.cpp>
+#include <moc_enum.cpp>
+#include <moc_global.cpp>
+#include <moc_model.cpp>
+#include <moc_user_account.cpp>
 
 #endif
