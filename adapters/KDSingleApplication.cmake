d82610e55acf942f271bd92d3d495aa218148aed
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2026-09-18 10:29:30 +0800

    Allow using system dependencies
    So downstream packagers/users can skip building them and reuse the installed one if possible.

--- adapters/KDSingleApplication.cmake
+++ adapters/KDSingleApplication.cmake
@@ -1,9 +1,12 @@
-if(NOT LITO_CMAKE_DEPENDENCY_MODE STREQUAL "source")
-  message(FATAL_ERROR "KDSingleApplication requires its source")
+if(LITO_CMAKE_DEPENDENCY_MODE STREQUAL "find")
+  find_package(KDSingleApplication-qt6 REQUIRED)
+  add_library(kdsingleapplication ALIAS KDAB::kdsingleapplication)
+elseif(LITO_CMAKE_DEPENDENCY_MODE STREQUAL "source")
+  add_subdirectory("${LITO_CMAKE_DEPENDENCY_SOURCE_DIR}"
+                   "${CMAKE_CURRENT_BINARY_DIR}/kd-single-application" EXCLUDE_FROM_ALL)
+else()
+  message(FATAL_ERROR "Unsupported lito cmake dependency mode: ${LITO_CMAKE_DEPENDENCY_MODE}")
 endif()
-
-add_subdirectory("${LITO_CMAKE_DEPENDENCY_SOURCE_DIR}"
-                 "${CMAKE_CURRENT_BINARY_DIR}/kd-single-application" EXCLUDE_FROM_ALL)
 
 if(NOT TARGET kdsingleapplication)
   message(FATAL_ERROR "KDSingleApplication did not provide kdsingleapplication")
--- adapters/Random.cmake
+++ adapters/Random.cmake
@@ -1,9 +1,11 @@
-if(NOT LITO_CMAKE_DEPENDENCY_MODE STREQUAL "source")
-  message(FATAL_ERROR "random requires its source")
+if(LITO_CMAKE_DEPENDENCY_MODE STREQUAL "find")
+  find_package(effolkronium_random REQUIRED)
+elseif(LITO_CMAKE_DEPENDENCY_MODE STREQUAL "source")
+  add_subdirectory("${LITO_CMAKE_DEPENDENCY_SOURCE_DIR}"
+                   "${CMAKE_CURRENT_BINARY_DIR}/random" EXCLUDE_FROM_ALL)
+else()
+  message(FATAL_ERROR "Unsupported lito cmake dependency mode: ${LITO_CMAKE_DEPENDENCY_MODE}")
 endif()
-
-add_subdirectory("${LITO_CMAKE_DEPENDENCY_SOURCE_DIR}"
-                 "${CMAKE_CURRENT_BINARY_DIR}/random" EXCLUDE_FROM_ALL)
 
 if(NOT TARGET effolkronium_random)
   message(FATAL_ERROR "random did not provide effolkronium_random")
